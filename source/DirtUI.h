fa35214a276ab6b9f33a8728944414ec4e679f16
asb2m10 <asb2m10@gmail.com>
2021-12-16 10:39:05 -0500

    WIP 5

---
+++ source/DirtUI.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "PluginProcessor.h"
+
+
+class OrbitViewer : public juce::Component {
+
+
+};
+
+class StatusBar : juce::Component {
+public:
+    void paint (juce::Graphics& g) override {
+
+    }
+};
